17974cf0283fd8128c88cc4780930c7d45110215
Aurelio Grott Neto <aureliogrott@Aurelios-MacBook-Pro.local>
2017-03-16 01:38:34 -0300

    populate board with dead ants and added a function that generates a list of live ants

--- ants.py
+++ ants.py
@@ -7,12 +7,18 @@ class ant:
 		self.position = position
 		self.loaded = loaded
 
-def generate_board(size):
-	return np.array([[0 for x in range(size)] for y in range(size)])
+def generate_board(lines, columns):
+	return np.zeros(shape=(lines, columns))
 
 def populate_board(board, n_dead_ants):
-	board_size = board.size()
+	board_columns = board.shape[1]
+	board_size = board.size
 	dead_ants_positions = random.sample(range(board_size), n_dead_ants)
 	dead_ants_coord = []
-	for i in dead_ants_positions:
-		
+	for i in dead_ants_positions:	dead_ants_coord.append((i/board_columns, i%board_columns))
+	for i in dead_ants_coord:	board[i]= -1
+
+def generate_live_ants(board_size, board_columns, n_live_ants):
+	return [ant(k, False) for k in [(i/board_columns, i%board_columns) for i in random.sample(range(board_size), n_live_ants)]]
+
+print [k.position for k in generate_live_ants(25, 5, 10)]

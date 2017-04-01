d28dc0379251b46ca6eeb5be2c61157cb4194b4e
Aurelio Grott Neto <grott.aurelio@gmail.com>
2017-04-01 16:11:10 -0300

    resolved conflicts

--- truezera/Ant.h
+++ truezera/Ant.h
@@ -3,17 +3,15 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
 
 #define FRACASSO 0
 #define SUCESSO 1
 
 #define NAO 0
 #define SIM 1
-
-#define BOARD_SIZE 160
-#define N_LIVE_ANTS 100
-#define N_DEAD_ANTS 7000
-#define FOV_RANGE 5
 
 typedef struct Pair
 {
@@ -40,15 +38,15 @@ typedef struct Pair *pPair, **ppPair;
 typedef struct Data *pData, **ppData;
 
 
-int generate_live_ants_array(ppAnt pp, int n_live_ants, int fov_range);
+int generate_live_ants_array(ppAnt pp, int n_live_ants, int fov_range, int BOARD_SIZE);
 int show_ants_array(pAnt p, int n_ants);
-int populate_board(int ***board, int n_dead_ants);
-int generate_live_ants_board(int ***board, int n_live_ants, pAnt ants_array);
+int populate_board(int ***board, int n_dead_ants, int BOARD_SIZE);
+int generate_live_ants_board(int ***board, int n_live_ants, pAnt ants_array, int BOARD_SIZE);
 void pick_up_ant(int ***board, pAnt p);
 void drop_off_ant(int ***board, pAnt p);
-int should_pick_up(int **board, struct Ant ant);
-int should_drop_off(int **board, struct Ant ant);
-int move(int **board, int **live_ants_board, pPair coords, pPair old_position, pPair new_position, pAnt p);
+int should_pick_up(int **board, struct Ant ant, int BOARD_SIZE);
+int should_drop_off(int **board, struct Ant ant, int BOARD_SIZE);
+int move(int **board, int **live_ants_board, pPair coords, pPair old_position, pPair new_position, pAnt p, int BOARD_SIZE);
 
 
 int generate_data_board_for_plot(int ***board, int n_data, pData data_array);
@@ -57,7 +55,7 @@ int generate_data_array(ppData pp, int n_data);
 
 /**     UTILS FUNCTIONS     **/
 int generate_coords(ppPair pp);
-int generate_random_numbers(int **random_numbers, int n_numbers);
-int generate_board(int ***board);
-int show_board(int **board, char *board_name);
+int generate_random_numbers(int **random_numbers, int n_numbers, int BOARD_SIZE);
+int generate_board(int ***board, int BOARD_SIZE);
+int show_board(int **board, char *board_name, int BOARD_SIZE);
 int mod (int a, int b);
